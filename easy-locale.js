39daf3165f407c38923b26f0cc9f8016295a2ad6
Vova <v@vforvova.ru>
2015-03-29 22:55:09 +0300

    Cosmetics

--- easy-locale.js
+++ easy-locale.js
@@ -32,10 +32,10 @@ var locale = (function () {
         }
       }
     };
-    if(pluralizationRules[locale]){
+    if (pluralizationRules[locale]) {
       return pluralizationRules[locale](count);
-    }else{
-      throw new Error('No pluralization rules for '+locale+' locale');
+    } else {
+      throw new Error('No pluralization rules for ' + locale + ' locale');
     }
   }
 
@@ -48,22 +48,21 @@ var locale = (function () {
     return string;
   }
 
-  function isObject(hash){
-    return hash !== null && typeof hash === 'object'
+  function isObject(hash) {
+    return hash !== null && typeof hash === 'object';
   }
 
   return {
-    init: function(userLocale, data, multipleType){
+    init: function (userLocale, data, multipleType) {
       locale = userLocale;
       translations = data;
       multiple = multipleType ? multipleType : true;
     },
     t: function (key, data) {
-      if(!data){
-        data = {};
-      }
+      if (!data) data = {};
       try {
         var e, hash, i, keys, rand, result;
+
         keys = key.split('.');
         i = 0;
         hash = multiple ? translations[locale] : translations;
@@ -82,10 +81,10 @@ var locale = (function () {
             cache[key] = result;
           }
         } else if (isObject(hash)) {
-          if (data['count'] === void 0) {
+          if (data.count === void 0) {
             throw new Error("doesn't have 'count' parameter");
           } else {
-            hash = hash[pluralization(locale, data['count'])];
+            hash = hash[pluralization(locale, data.count)];
             result = replaceInterpolated(hash, data);
           }
         } else {
